e25e2c6c152fad192f3599a5e8bdf52ba0d33710
Jonnboy91 <jon.nesten@gmail.com>
2021-08-23 11:56:12 +0300

    Task 1 is done (First App)

---
+++ components/ListItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Text, View, Image, TouchableOpacity} from 'react-native';
+
+const ListItem = (props) => {
+    return (
+        <TouchableOpacity style={{marginBottom: 8, backgroundColor: "grey"}}>
+            <View style={{flexDirection: "row", margin: 12}}>
+                <Image
+                    style={{width: 200, height: 300}}
+                    source={{uri: props.singleMedia.thumbnails.w160}}
+                />
+                <View style={{paddingLeft: 8, paddingTop: 8, width: 150}}>
+                    <Text style={{fontSize: 16}}>{props.singleMedia.title}</Text>
+                    <Text style={{fontSize: 12}}>{props.singleMedia.description}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+ListItem.propTypes = {
+    singleMedia: PropTypes.object,
+};
+
+export default ListItem;
