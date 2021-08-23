152a805027b6cf512c72275eb58b09859df1ed1c
Jonnboy91 <jon.nesten@gmail.com>
2021-08-23 15:33:57 +0300

    Style updated

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,23 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Text, View, Image, TouchableOpacity} from 'react-native';
+import {Text, View, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
 const ListItem = (props) => {
     return (
-        <TouchableOpacity style={{marginBottom: 8, backgroundColor: "grey"}}>
-            <View style={{flexDirection: "row", margin: 12}}>
+        <TouchableOpacity style={styles.MainBox}>
+            <View style={styles.imagebox}>
                 <Image
-                    style={{width: 200, height: 300}}
-                    source={{uri: props.singleMedia.thumbnails.w160}}
+                    style={styles.image}
+                    source={{uri: props.singleMedia.thumbnails?.w160 ?? 'https://placekitten.com/200/300'}}
                 />
-                <View style={{paddingLeft: 8, paddingTop: 8, width: 150}}>
-                    <Text style={{fontSize: 16}}>{props.singleMedia.title}</Text>
-                    <Text style={{fontSize: 12}}>{props.singleMedia.description}</Text>
-                </View>
             </View>
+            <View style={styles.textBox}>
+                <Text style={{fontSize: 16}}>{props.singleMedia.title}</Text>
+                <Text style={{fontSize: 12}}>{props.singleMedia.description}</Text>
+            </View>
+
         </TouchableOpacity>
     );
 }
+
+const styles = StyleSheet.create({
+    MainBox: {
+        flexDirection: 'row',
+        padding: 15,
+        marginBottom: 5,
+        backgroundColor: '#eee',
+        borderRadius: 6,
+        flex: 1,
+    },
+    imagebox: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        borderRadius: 6,
+    },
+    textBox: {
+        flex: 2,
+        padding: 10,
+    }
+});
 
 ListItem.propTypes = {
     singleMedia: PropTypes.object,
