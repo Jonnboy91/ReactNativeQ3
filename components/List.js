afe60597b778108c0ba6b9964ed44b0ae760c78e
Jonnboy91 <jon.nesten@gmail.com>
2021-08-24 11:26:55 +0300

    Better place for Try/Catch

--- components/List.js
+++ components/List.js
@@ -5,33 +5,35 @@ import ListItem from './ListItem'
 const url = 'http://media.mw.metropolia.fi/wbma/';
 
 const List = () => {
-  const [data, setData] = useState({ mediaArray: [] });
+  const [data, setData] = useState({mediaArray: []});
 
-  try {
-    useEffect(() => {
-      const loadMedia = async () => {
-        const response = await fetch(url+'media');
+
+  useEffect(() => {
+    const loadMedia = async () => {
+      try {
+        const response = await fetch(url + 'media');
         const jsonResponse = await response.json();
         const json = await Promise.all(jsonResponse.map(async (item) => {
-          const response =  await fetch(url + 'media/' + item.file_id);
+          const response = await fetch(url + 'media/' + item.file_id);
           const json = await response.json();
-          console.log("Tässä",json.thumbnails?.w160 ?? "https://placekitten.com/200/300");
+          console.log("Tässä", json.thumbnails?.w160 ?? "https://placekitten.com/200/300");
           return json;
         }));
         setData(json);
-      };
-      loadMedia();
-    }, []);
-  } catch {
-    console.log(error);
-  }
+      } catch {
+        console.log(error);
+      }
+    };
+    loadMedia();
+  }, []);
+
 
   return (
-    <FlatList 
-          data={ data }
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({item}) => <ListItem singleMedia={item} />}
-        />
+    <FlatList
+      data={data}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({item}) => <ListItem singleMedia={item} />}
+    />
   );
 };
 
