f25248cba2c6d0cc71df3ee0f01df57f51d0d525
Jonnboy91 <jon.nesten@gmail.com>
2021-08-27 11:02:34 +0300

    Changed the icon and text box from outside the imageBackground to inside that view

--- views/Home.js
+++ views/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Settings } from "react-native-feather";
+import {Settings} from "react-native-feather";
 import {StatusBar} from 'expo-status-bar'
 import {StyleSheet, SafeAreaView, ImageBackground, View, Text} from 'react-native';
 import GlobalStyles from '../GlobalStyles'
@@ -8,14 +8,11 @@ import List from '../components/List'
 const Home = ({navigation}) => {
     return (
         <SafeAreaView style={[styles.container, GlobalStyles.droidSafeArea]}>
-            <View style={styles.textBox}>
-                <Text style={{
-        fontSize: 30,}}>Hello</Text>
-            </View>
-            <Settings style={styles.settingsIcon} stroke="white" width={32} height={32}/>
             <View style={styles.ImageBackgroundBox}>
-            <ImageBackground source={{uri: 'https://placekitten.com/250/200'}} style={styles.backgroundImageView} imageStyle={styles.backgroundImage}>
-            </ImageBackground>
+                <ImageBackground source={{uri: 'https://placekitten.com/250/200'}} style={styles.backgroundImageView} imageStyle={styles.backgroundImage}>
+                    <Text style={styles.textBox}>Hello</Text>
+                    <Settings style={styles.settingsIcon} stroke="white" width={32} height={32} />
+                </ImageBackground>
             </View>
             <List navigation={navigation}></List>
             <StatusBar style="auto" />
@@ -33,24 +30,24 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     backgroundImageView: {
-        width: '100%', 
+        width: '100%',
         height: '100%',
         resizeMode: 'cover',
         marginBottom: 16,
-        
+
     },
     backgroundImage: {
         borderBottomRightRadius: 100
     },
-    
     textBox: {
+        fontSize: 30,
         position: 'absolute',
         backgroundColor: 'blue',
         width: 70,
-        top: 200,
+        bottom: 20,
         zIndex: 9999,
     },
-    settingsIcon:{
+    settingsIcon: {
         position: 'absolute',
         top: 20,
         right: 20,
